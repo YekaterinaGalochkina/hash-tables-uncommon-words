04572b485ea1275878734a8f8283f6b30a8849b6
Yekaterina Galochkina <galocichina.ecaterina@gmail.com>
2025-04-25 11:40:32 -0700

    Implementing Original function in 2 methods, plain python and hashtable

--- activity/original.py
+++ activity/original.py
@@ -1,2 +1,39 @@
 def uncommon_from_sentences(s1, s2):
-    pass
+    list1 = s1.split()
+    list2 = s2.split()
+
+    list = list1 + list2
+
+    unique_words = []
+
+    for word in list:
+        if list.count(word) == 1:
+            unique_words.append(word)
+            
+    return unique_words
+
+print(uncommon_from_sentences("this apple is sweet","this apple is sour")) # expected = ["sweet", "sour"]
+
+
+def uncommon_from_sentences(s1, s2):
+    list1 = s1.split()
+    list2 = s2.split()
+
+    all_words = list1 + list2
+
+    word_freq = {}  # hash table
+
+    for word in all_words:
+        if word in word_freq:
+            word_freq[word] += 1
+        else:
+            word_freq[word] = 1
+
+    unique_words = []
+    for word in word_freq:
+        if word_freq[word] == 1:
+            unique_words.append(word)
+
+    return unique_words
+
+print(uncommon_from_sentences("this apple is sweet","this apple is sour")) # expected = ["sweet", "sour"]
